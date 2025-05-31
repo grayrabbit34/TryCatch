9bc879cd7db0feb668f4f6b4923dbeef9e7d60fd
BURAK DÜLGEROĞLU <dulgerogluburak04@gmail.com>
2025-05-31 18:19:36 +0700

    TryCatch

---
+++ TryCatch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Lütfen bir sayı giriniz: ");
+        while (true)
+        {
+            try
+            {
+                double number = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"Girdiğiniz sayının karesi: {Math.Pow(number, 2)}");
+                break;
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+            }
+            //finally bloğunu burada ihtiyaç olmadığı için kullanmadım
+        }
+    }
+}
